4b8b8a70d526b4165708dd5552a6dc7d69642020
Squili <squili@tutanota.com>
2020-11-28 09:33:58 -0800

    fix aerich

--- migrations/models/1_20201128083202_banner_id_not_null.sql
+++
@@ -1,4 +0,0 @@
-##### upgrade #####
-ALTER TABLE "banutilbanevent" ALTER COLUMN "banner_id" SET NOT NULL;
-##### downgrade #####
-ALTER TABLE "banutilbanevent" ALTER COLUMN "banner_id" DROP NOT NULL;
